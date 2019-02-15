6f4a68dc0c936f57ae95981a25da4cee10320a31
javier edgardo ciceri <javibishop@gmail.com>
2019-02-15 19:27:48 -0300

    s

---
+++ gidas2/reactredux/ClientApp/src/components/ConsejeriaNew.js
@@ -0,0 +1,186 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { FormControl, Button, Grid, Row, Col, Label, PageHeader } from 'react-bootstrap'; 
+import { actionCreators } from '../store/ConsejeriaNew';
+
+class ConsejeriaNew extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+        this.props.loadUsuaries();
+        this.props.newConsejeria();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.redirectEdit) {
+            this.props.history.push(`/consejeria/${nextProps.idConsejeria}`);
+        }
+    }
+
+    render() {
+    return (
+        <div>
+            <PageHeader>
+                Nueva Consejeria
+            </PageHeader>
+            <Row className="show-grid">
+                <Col xs={12} md={12}>
+                    {renderNuevaConsejeria(this.props)} 
+                </Col>
+            </Row>
+            <Row className="show-grid">
+                <Col xs={6} md={6}></Col>
+             </Row>
+      </div>
+    );
+  }
+}
+
+function renderNuevaConsejeria(props) {
+    const { handleChangeConsejeriaNew, saveConsejeriaNew } = props;
+    return (
+        <Grid fluid={true} bsClass="container" className='form'>
+            <Row>
+                <Col xs={12} md={12}>
+                    <Row className="show-grid">
+                        <Col xs={6} md={6}>
+                            <Label>Profesional 1</Label>
+                            <FormControl
+                                value={props.consejeriaNew.usuarie1Id || ''}
+                                id="usuarie1Id"
+                                onChange={handleChangeConsejeriaNew}
+                                componentClass="select" placeholder="select">
+                                {props.usuaries.map((option, index) =>
+                                    <option value={option.value} key={index}> {option.label} </option>)}
+                            </FormControl>
+                        </Col>
+                        <Col xs={6} md={6}>
+                            <Label>Profesional 2</Label>
+                            <FormControl
+                                value={props.consejeriaNew.usuarie2Id || ''}
+                                id="usuarie2Id"
+                                onChange={handleChangeConsejeriaNew}
+                                componentClass="select" placeholder="select">
+                                {props.usuaries.map((option, index) =>
+                                    <option value={option.value} key={index}> {option.label} </option>)}
+                            </FormControl>
+                        </Col>
+                    </Row>
+                    
+                    <Row className="show-grid">
+                        <Col xs={6} md={6}>
+                            <Label>Nombre</Label>
+                            <FormControl
+                                id="nombre"
+                                type="text"
+                                label="Nombre"
+                                placeholder="Ingrese su nombre"
+                                value={props.consejeriaNew.nombre || ''}
+                                onChange={handleChangeConsejeriaNew}
+                            />
+                        </Col>
+                        <Col xs={6} md={6}>
+                            <Label>Apellido</Label>
+                            <FormControl
+                                id="apellido"
+                                type="text"
+                                label="Apellido"
+                                placeholder="Ingrese su apellido"
+                                value={props.consejeriaNew.apellido || ''}
+                                onChange={handleChangeConsejeriaNew}
+                            />
+                        </Col>
+                    </Row>
+                    <Row className="show-grid">
+                        <Col xs={4} md={4}>
+                            <Label>Edad</Label>
+                            <FormControl
+                                id="edad"
+                                type="number"
+                                label="edad"
+                                placeholder="Ingrese su Edad"
+                                value={props.consejeriaNew.edad || 0}
+                                onChange={handleChangeConsejeriaNew}
+                            />
+                        </Col>
+                        <Col xs={4} md={4}>
+                            <Label>Fecha Nacimiento</Label>
+                            <FormControl
+                                id="fechaNacimiento"
+                                type="date"
+                                label="Fecha Nacimiento"
+                                placeholder="Ingrese su Fecha Nacimiento"
+                                value={props.consejeriaNew.fechaNacimiento || ''}
+                                onChange={handleChangeConsejeriaNew}
+                            />
+                        </Col>
+                        <Col xs={4} md={4}>
+                            <Label>Nacionalidad</Label>
+                            <FormControl
+                                value={props.consejeriaNew.nacionalidadId || ''}
+                                id="nacionalidadId"
+                                onChange={handleChangeConsejeriaNew}
+                                componentClass="select" placeholder="select">
+                                <option value="1">Argentine</option>
+                                <option value="2">Paraguaye</option>
+                                <option value="3">Boliviane</option>
+                            </FormControl>
+                        </Col>
+                    </Row>
+                    <Row className="show-grid">
+                        <Col xs={4} md={4}>
+                            <Label>Telefono</Label>
+                            <FormControl
+                                id="telefono"
+                                type="text"
+                                label="Telefono"
+                                placeholder="Ingrese su Telefono"
+                                value={props.consejeriaNew.telefono || ''}
+                                onChange={handleChangeConsejeriaNew}
+                            />
+                        </Col>
+                        <Col xs={8} md={8}>
+                            <Label>Direccion</Label>
+                            <FormControl
+                                id="direccion"
+                                type="text"
+                                label="direccion"
+                                placeholder="Ingrese su Direccion"
+                                value={props.consejeriaNew.direccion || ''}
+                                onChange={handleChangeConsejeriaNew}
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={6}>
+                            <Row className='pull-right'>
+                                <Button className='btn-sm btn-success pull-rigth' onClick={() => { saveConsejeriaNew(props.consejeriaNew) }}>  Guardar </Button>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Grid>
+    );
+}
+
+//const mapDispatchToProps = (dispatch, ownProps) => ({
+//    //handleSubmit: () => dispatch(saveConsejeria(ownProps.formName))
+//    //handleSubmit: () => dispatch({ type: 'NAME_CHANGED', payload: name }),
+//})
+
+//const mapStateToProps = (state)  => ({
+//    state : state.consejeriaNewDto
+//})
+
+//export default connect(mapStateToProps, mapDispatchToProps)(ConsejeriaEdit);
+
+
+
+export default connect(
+    state => state.consejeriaNew,
+    dispatch => bindActionCreators(actionCreators, dispatch)
+)(ConsejeriaNew);
---
+++ gidas2/reactredux/ClientApp/src/store/ConsejeriaNew.js
@@ -0,0 +1,162 @@
+﻿const initNewConsejeria = 'INIT_NEW_CONSEJERIA';
+
+const getUsuariesRequest = 'GET_USUARIES_RUEQUEST';
+const getUsuariesSuccess = 'GET_USUARIES_SUCCESS';
+
+const saveConsejeriaNewRequest = 'saveConsejeriaNewRequest';
+const saveConsejeriaNewFailure = 'saveConsejeriaNewFailure';
+const saveConsejeriaNewSuccess = 'saveConsejeriaNewSuccess';
+
+const changeStateBeforeConsejeriaNew = 'ChangeStateBeforeConsejeriaNew';
+const changingStateConsejeriaNew = 'ChangingStateConsejeriaNew';
+const changeStateAfterConsejeriaNew = 'ChangeStateAfterConsejeriaNew';
+
+const redirectEdit = 'RedirectEdit';
+
+const initState = {
+        "id": 0,
+        "numero": 0,
+        "fechaIngreso": new Date(),
+        "usuariaId": 0,
+        "usuarie1Id": 0,
+        "usuarie2Id": 0,
+        "observaciones": "",
+        "nivelInstruccion": 0,
+        "nivelInstruccionEstado": 0,
+        "nombre": "",
+        "apellido": "",
+        "edad": 0,
+        "fechaNacimiento": new Date(),
+        "nacionalidadId": 0,
+        "telefono": "",
+        "direccion": ""
+};
+const initialState = {
+    consejeriaNew: initState, usuaries: [], isLoading: false, isChanging: false, redirectEdit: false, idConsejeria: 0
+};
+
+export const actionCreators = {
+    loadUsuaries : () => async (dispatch, getState) => {
+
+        dispatch({ type: getUsuariesRequest });
+
+        const url = `api/Usuaries/GetForCombo`;
+        const response = await fetch(url);
+        const usuaries = await response.json();
+
+        dispatch({ type: getUsuariesSuccess, usuaries });
+    },
+
+    setRedirecting: value => (dispatch, getState) => {
+        dispatch({ type: 'SetRedirect', value});
+    },
+
+    newConsejeria: id => async (dispatch, getState) => {
+        dispatch({ type: initNewConsejeria, getState });
+    },
+
+    saveConsejeriaNew: consejeriaNew => async (dispatch) => {
+        const url = `api/Consejerias/PostConsejeriaNew`;
+        const settings = {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json;',
+            },
+
+            body: JSON.stringify(consejeriaNew)
+        };
+
+        dispatch({ type: saveConsejeriaNewRequest });
+
+        try {
+            const response = await fetch(url, settings);
+            const id = await response.json();
+            dispatch({ type: saveConsejeriaNewSuccess, id });
+            //si id == 0 hacer redirect a edicion.
+            //if (consejeriaNew.id == 0)
+            //    props.history.push(`/consejeria/${consejeria.consejeriaDto.id}`);
+            //    dispatch({ type: 'RedirectEdit', consejeria });
+        } catch (error) {
+            dispatch({ type: saveConsejeriaNewFailure});
+        }
+    },
+
+      
+    handleChangeConsejeriaNew: valor => (dispatch, getState) => {
+        dispatch({ type: changeStateBeforeConsejeriaNew, valor });
+        dispatch({ type: changingStateConsejeriaNew, valor });
+        dispatch({ type: changeStateAfterConsejeriaNew, valor });
+    },
+};
+
+export const reducer = (state, action) => {
+  state = state || initialState;
+
+    if (action.type === saveConsejeriaNewSuccess) {
+        return {
+            ...state,
+            idConsejeria: action.id,
+            isLoading: false,
+            redirectEdit: true
+        };
+    }
+
+    if (action.type === initNewConsejeria) {
+        return {
+            ...state,
+            consejeriaNew: initialState,
+            isLoading: false
+        };
+    }
+
+    if (action.type === getUsuariesRequest) {
+        return {
+            ...state,
+            isLoading: true
+        };
+    }
+    if (action.type === getUsuariesSuccess) {
+        return {
+            ...state,
+            usuaries: action.usuaries,
+            isLoading: false
+        };
+    }
+
+
+    if (action.type === changeStateBeforeConsejeriaNew) {
+        return {
+            ...state,
+            isChanging: true
+        };
+    }
+   
+
+    if (action.type === changeStateBeforeConsejeriaNew) {
+        return {
+            ...state,
+            isChanging: true
+        };
+    }
+    if (action.type === changingStateConsejeriaNew) {
+        if (action.valor.target.type === "checkbox") {
+            state.consejeriaNew[action.valor.target.id] = action.valor.target.checked;
+        } else {
+            state.consejeriaNew[action.valor.target.id] = action.valor.target.value;
+        }
+        
+        return {
+            ...state,
+            isChanging: true,
+            consejeriaNew: state.consejeriaNew
+        }
+    }
+    if (action.type === changeStateAfterConsejeriaNew) { 
+        return {
+            ...state,
+            consejeriaNew: state.consejeriaNew,
+            isChanging: false
+        };
+    }
+  return state;
+};
